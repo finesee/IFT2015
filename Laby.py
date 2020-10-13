2ca490cd5d0af72da322b44fac2cabbc44887146
GitHub User Name <email>
2020-10-13 13:42:44 -0400

    adding files

---
+++ Laby.py
@@ -0,0 +1,124 @@
+# zhimingzeng_laurasanchezfernandez
+
+class Empty(Exception):
+    """Error attempting to access an element from an empty container."""
+    pass
+
+class Stack:
+    """LIFO Stack implementation using a Python list as underlying storage."""
+    def __init__(self):
+        """Create an empty stack"""
+        self._data = []         # nonpublic list instance
+
+    def __len__(self):
+        """Return the number of elements in the stack"""
+        return len(self._data)
+
+    def is_empty(self):
+        """Return True if the stack is empty"""
+        return len(self._data) == 0
+
+    def push(self, e):
+        """Add element e to the top of the stack"""
+        self._data.append(e)            # new item stored at the end of list
+
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack
+
+        Raise Empty Exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]       # the last item in the list
+
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e., LIFO).
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()     # remove last item from list
+
+class Laby(object):
+    """Solve a maze using the solve() method."""
+    def __init__(self, lab, dim):
+        """Initialize the labyrinth object."""
+        if len(lab) != dim * dim:
+            raise ValueError("lab's length doesn't equal dim^2 !")
+        self._lab = lab
+        self._dim = dim
+        self._maze = []
+        self.start = ()
+        self.end = ()
+
+    def init_maze(self):
+        """Transform the labyrinth string to a 2-dimensional list,
+
+        and confirm the starting point and finishing point if possible.
+        """
+        for j in range(self._dim):
+            """Separate for dim lists"""
+            list = []
+            for i in range(self._dim):
+                """Separate each item in the current list"""
+                c = self._lab[j * self._dim + i]  # Get the current item
+                if c == 'D':  # Check if it's the starting point
+                    self.start = (j, i)
+                elif c == 'F':  # Check if it's the finishing point
+                    self.end = (j, i)
+                list.append(c)  # Get the current list and append it to the final 2-dimensional list
+            self._maze.append(list)  # Get the whole two dimensional list
+        # self._maze = maze
+
+    def show_maze(self):
+        for list in self._maze:
+            for item in list:
+                print(item, end=" ")
+            print()
+        print("Starting point is {0}; finishing point is {1}".format(self.start, self.end))
+
+    def solve(self):
+        pass
+
+lab = "########D#000##000#0###0##F###000################"
+dim = 7
+
+laby = Laby(lab, dim)
+laby.show_maze()
+laby.init_maze()
+laby.show_maze()
+
+# import datetime
+# a = datetime.datetime.now()
+# b = datetime.datetime.now()
+# delta = b - a
+# print(delta)
+
+# str = "#####D0###F#####"
+# str = "########D#000##000#0###0##F###000################"
+# maz = []
+# dim = 7
+# for j in range(dim):
+#     """Separate for dim lists"""
+#     list = []
+#     for i in range(dim):
+#         """Separate each item in the current list"""
+#         c = str[j*dim+i]    # Get the current item
+#         if c == 'D':        # Check if the starting point
+#             start = (j, i)
+#         elif c == 'F':      # Check if the finishing point
+#             end = (j, i)
+#         list.append(c)      # Get the current list and append it
+#     maz.append(list)        # Get the whole two dimensional list
+# for list in maz:
+#     # print(list)
+#     for item in list:
+#          print(item, end = " ")
+#     print()
+
+# maz[1][0] = '0'
+# print(end)
+# print(start)
+# print(maz[0][1])
+# print(start[0], start[1], end[0], end[1])
---
+++ PostToIn.py
@@ -0,0 +1,86 @@
+# zhimingzeng_laurasanchezfernandez
+
+class Empty(Exception):
+    """Error attempting to access an element from an empty container."""
+    pass
+
+class Stack:
+    """LIFO Stack implementation using a Python list as underlying storage."""
+    def __init__(self):
+        """Create an empty stack"""
+        self._data = []         # nonpublic list instance
+
+    def __len__(self):
+        """Return the number of elements in the stack"""
+        return len(self._data)
+
+    def is_empty(self):
+        """Return True if the stack is empty"""
+        return len(self._data) == 0
+
+    def push(self, e):
+        """Add element e to the top of the stack"""
+        self._data.append(e)            # new item stored at the end of list
+
+    def top(self):
+        """Return (but do not remove) the element at the top of the stack
+
+        Raise Empty Exception if the stack is empty
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data[-1]       # the last item in the list
+
+    def pop(self):
+        """Remove and return the element from the top of the stack (i.e., LIFO).
+
+        Raise Empty exception if the stack is empty.
+        """
+        if self.is_empty():
+            raise Empty('Stack is empty')
+        return self._data.pop()     # remove last item from list
+
+def postfixToInfix(exp):
+    """Receives a String composed of operators and operands in postfixed order,
+
+    and returns the equivalent expression in infixed order.
+    """
+    operatorList1 = ['+', '-', '*', '/', '<', '>'] # Operator Type 1: pop() twice plus '(' + ')'
+    operatorList2 = ['=']                          # Operator Type 1: pop() twice without parentheses
+    operatorList3 = ['\u221A', '!']                # Operator Type 2: pop() once
+    stack = Stack()
+    str = exp.split()
+    # print(str)
+
+    for c in str:
+        if c not in operatorList1 and c not in operatorList2 and c not in operatorList3:
+            """if not operator, push to stack"""
+            stack.push(c)
+        elif c in operatorList1:
+            """if Operator Type 1: pop() twice for two oprands, 
+            append with this operator, plus '(' + ')'
+            """
+            operator1 = stack.pop()
+            operator2 = stack.pop()
+            stack.push("(" + operator2 + c + operator1 + ")")
+        elif c in operatorList2:
+            """if Operator Type 2: pop() twice for two oprands, append with this operator"""
+            operator1 = stack.pop()
+            operator2 = stack.pop()
+            stack.push(operator2 + c + operator1)
+        elif c in operatorList3:
+            """if Operator Type 3: pop() once for one oprand, append with this operator"""
+            operator = stack.pop()
+            stack.push(c + operator)
+    return stack.pop()
+
+
+exp = "9 √ 3 ="
+print(postfixToInfix(exp))
+exp = "15 7 1 1 + - / 3 * 2 1 1 + + -"
+print(postfixToInfix(exp))
+exp = "n ! n 1 + ! <"
+print(postfixToInfix(exp))
+
+
+
